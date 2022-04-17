a950ae05e9a4929175feff9dfb699b464683e3ed
Kristina Zhukova <KristikYar@mail.ru>
2022-04-18 00:54:49 +0300

    task-async

--- src/Async.ts
+++ src/Async.ts
@@ -1,8 +1,15 @@
+import { resolve } from 'dns';
+
 /*
 Создайте функцию mock, которая принимает на вход аргумент number (количество миллисекунд) и возвращает Promise,
 который завершится через заданное количество миллисекунд со значением, переданным в аргумент.
  */
 export function mock(ms: number): Promise<number> {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(ms);
+        }, ms);
+    });
 }
 
 /*
@@ -10,30 +17,18 @@ export function mock(ms: number): Promise<number> {
  */
 export function getData(): Promise<number[]> {
     const result: number[] = [];
-
-    return mock(100)
-        .then((data1) => {
-            result.push(data1);
-            return mock(200);
-        })
-        .then((data2) => {
-            result.push(data2);
-            return mock(300);
-        })
-        .then((data3) => {
-            result.push(data3);
-            return result;
-        });
+    return Promise.all([mock(100), mock(200), mock(300)]);
 }
 
 /*
 Исправьте функцию catchException так, чтобы блок try/catch обрабатывал
 завершенный с ошибкой Promise и возвращал текст ошибки.
- */
+*/
+
 export async function catchException(): Promise<string | undefined> {
     try {
-        Promise.reject(new Error('my error'));
-    } catch (err) {
+        await Promise.reject(new Error('my error'));
+    } catch (err: any) {
         return err.message;
     }
 }
